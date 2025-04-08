84bd2221e0d7ac8c6b4a65c4a56c4009b87dee85
Ankur Mishra <2018ankurm@gmail.com>
2025-04-08 12:00:06 -0400

    inititial graph

---
+++ graph.py
@@ -0,0 +1,9 @@
+class KnowledgeGraph:
+    def __init__(self) -> None:
+        pass
+
+    def add_entity(name: str) -> None:
+        pass
+
+    def add_edge(subject: str, relationship: str, object: str) -> None:
+        pass
